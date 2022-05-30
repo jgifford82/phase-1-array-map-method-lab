905dbd2d62d74e1e4b9acbbf8279cacd5cda6e71
Jennifer Gifford <jgifford82@gmail.com>
2022-05-30 10:15:19 -0600

    commit tree 4056deed0fde0056c2af6190898dab792849452d

--- index.js
+++ index.js
@@ -11,6 +11,80 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+// const titleCased = () => {
+//   return tutorials
+// }
+
+const titleCased = function() {
+  return (tutorials.map(str => str.split(' ').map(function (word) {
+    return (word.charAt(0).toUpperCase() + word.slice(1));
+  }).join(' ')))
 }
+
+// const titleCased = function(array) {
+//   return (array.map(str => str.split(' ').map(function (word) {
+//     return (word.charAt(0).toUpperCase() + word.slice(1));
+//   }).join(' ')))
+// }
+
+// const titleCased = function(array) {
+//   console.log(array.map(str => str.split(' ').map(function (word) {
+//     return (word.charAt(0).toUpperCase() + word.slice(1));
+//   }).join(' ')))
+//   // return tutorials
+// }
+
+// const titleCased = function(array) {
+//   return(array.map(str => str.split(' ').map(function (word) {
+//     return (word.charAt(0).toUpperCase() + word.slice(1));
+//   }).join(' ')))
+// }
+
+// const titleCased = function(array) {
+//   array.map(str => str.split(' ').map(function (word) {
+//     return (word.charAt(0).toUpperCase() + word.slice(1));
+//   }).join(' '));
+//   return tutorials
+// };
+
+// const titleCased = function(array) {
+//   array.map(function (str) {
+//   return str.split(' ').map(function (word) {
+//     return (word.charAt(0).toUpperCase() + word.slice(1));
+//   }).join(' '); 
+// });
+//   return tutorials
+// }
+
+// const titleCased = function() {
+//   tutorials.map(function (str) {
+//   return str.toLowerCase().split(' ').map(function (word) {
+//     return (word.charAt(0).toUpperCase() + word.slice(1));
+//   }).join(' '); 
+// }); 
+//   return tutorials;
+// }
+
+// const titleCased = tutorials.map(function (str) {
+//   return str.toLowerCase().split(' ').map(function (word) {
+//     return (word.charAt(0).toUpperCase() + word.slice(1));
+//   }).join(' '); 
+// }); 
+
+
+// Source: https://www.w3docs.com/snippets/javascript/how-to-convert-string-to-title-case-with-javascript.html
+// function toTitleCase(str) {
+//   return str.toLowerCase().split(' ').map(function (word) {
+//     return (word.charAt(0).toUpperCase() + word.slice(1));
+//   }).join(' ');
+// }
+
+// Source: Slack search. Returns strings in all uppercase.
+// const titleCased = () => {
+//   return tutorials.map((array) => {
+//     const newArray = array.split('')
+//     const newerArray = newArray.map((string) => string.charAt(0).toUpperCase() + string.slice(1).toLowerCase()
+//     )
+//     return newerArray.join('')
+//   })
+// }
